dec5074b1cab35c687ad321b1f810236600e67c9
ns01234 <fringenes1@gmail.com>
2024-03-22 06:03:14 +0300

    try

--- bucode24/increase_or_decrease.cpp
+++ bucode24/increase_or_decrease.cpp
@@ -16,8 +16,7 @@ int32_t main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     
-    int t;
-    cin >> t;
+    int t; cin >> t;
     while (t--) {
         int n, m;
         cin >> n >> m;
